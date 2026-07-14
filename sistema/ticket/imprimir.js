1b4b091f607b3a1fe1cfdb1255cd42224d359a93
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-14 19:44:58 -0400

    Update imprimir.js

--- sistema/ticket/imprimir.js
+++ sistema/ticket/imprimir.js
@@ -1,26 +1,7 @@
 window.TicketImprimir = {
-    imprimir(ticketData) {
+    imprimir: function(ticketData) {
         const ventana = window.open("", "_blank", "width=400,height=600");
-        
-        ventana.document.write(`
-            <html>
-                <head>
-                    <title>Imprimir Ticket ${ticketData.ticket}</title>
-                    <style>
-                        body { font-family: monospace; padding: 20px; }
-                    </style>
-                </head>
-                <body>
-                    ${ticketData.html}
-                    <script>
-                        window.onload = () => {
-                            window.print();
-                            window.close();
-                        };
-                    </script>
-                </body>
-            </html>
-        `);
+        ventana.document.write(`<html><body>${ticketData.html}<script>window.print();</script></body></html>`);
         ventana.document.close();
     }
 };
