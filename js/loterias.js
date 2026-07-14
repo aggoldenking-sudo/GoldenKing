64bd49efafbd371ec641738ded99fb95ef7220c7
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-13 20:03:35 -0400

    Update loterias.js

--- js/loterias.js
+++ js/loterias.js
@@ -1,1 +1,6 @@
-
+export const DATA_LOTERIAS = {
+    Granjita: { "00": "Ballena", "0": "Delfín", "01": "Carnero", "02": "Toro", "03": "Ciempiés", "04": "Alacrán", "05": "León", "06": "Rana", "07": "Perico", "08": "Ratón", "09": "Águila", "10": "Tigre", "11": "Gato", "12": "Caballo", "13": "Mono", "14": "Paloma", "15": "Zorro", "16": "Oso", "17": "Pavo", "18": "Burro", "19": "Chivo", "20": "Cochino", "21": "Gallo", "22": "Camello", "23": "Cebra", "24": "Iguana", "25": "Gallina", "26": "Vaca", "27": "Perro", "28": "Zamuro", "29": "Elefante", "30": "Caimán", "31": "Lapa", "32": "Ardilla", "33": "Pescado", "34": "Venado", "35": "Jirafa", "36": "Culebra" },
+    Guacharo: { "0": "Delfín", "00": "Ballena", "75": "Comodín" }, 
+    LottoActivo: { "00": "Ballena", "0": "Delfín" },
+    SelvaPlus: { "00": "Ballena", "0": "Delfín" }
+};
