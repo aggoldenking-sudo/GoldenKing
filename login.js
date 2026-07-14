d0ecba375132c028e3832b673f0af1b78200cf62
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-14 12:45:14 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,33 @@
+document
+.getElementById("entrar")
+.addEventListener("click", function(){
 
+
+    let usuario =
+    document.getElementById("usuario").value;
+
+
+    let clave =
+    document.getElementById("clave").value;
+
+
+
+    if(usuario === "admin" && clave === "1234"){
+
+
+        window.location.href = "sistema/index.html";
+
+
+    }else{
+
+
+        document
+        .getElementById("mensaje")
+        .textContent =
+        "Usuario o contraseña incorrectos";
+
+
+    }
+
+
+});
