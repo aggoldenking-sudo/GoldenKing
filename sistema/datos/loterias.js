8b5bbc8fe014f0b6ee3d8e56a8c7c944302c794a
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-15 14:30:05 -0400

    Add LottoActivo lottery with draw times

--- sistema/datos/loterias.js
+++ sistema/datos/loterias.js
@@ -1,12 +1,54 @@
 /*
 =====================================================
  GOLDEN KING
- LOTERÍAS REGISTRADAS
+ REGISTRO DE LOTERIAS
 =====================================================
 */
 
+
 window.LOTERIAS = [
 
-    "LottoActivo"
+
+    {
+
+        id:"LottoActivo",
+
+        nombre:"LOTTO ACTIVO",
+
+        activa:true,
+
+
+        sorteos:[
+
+
+            {
+                id:"LA-4PM",
+                nombre:"Lotto Activo",
+                hora:"04:00 PM",
+                activo:true
+            },
+
+
+            {
+                id:"LA-5PM",
+                nombre:"Lotto Activo",
+                hora:"05:00 PM",
+                activo:true
+            },
+
+
+            {
+                id:"LA-6PM",
+                nombre:"Lotto Activo",
+                hora:"06:00 PM",
+                activo:true
+            }
+
+
+        ]
+
+
+    }
+
 
 ];
