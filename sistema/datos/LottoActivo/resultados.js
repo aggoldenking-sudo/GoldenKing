ccc3fbf30f1564ef5f5dbe9336067775b74125b1
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-15 12:17:33 -0400

    Add Lotto Activo results data structure

--- sistema/datos/LottoActivo/resultados.js
+++ sistema/datos/LottoActivo/resultados.js
@@ -1,1 +1,57 @@
+/*
+=====================================================
+ GOLDEN KING
+ LOTTO ACTIVO
+ FUENTE DE RESULTADOS
+ sistema/datos/LottoActivo/resultados.js
+=====================================================
+*/
 
+
+window.LOTTO_ACTIVO_RESULTADOS = {
+
+
+    nombre:
+
+    "LOTTO ACTIVO",
+
+
+
+    tipo:
+
+    "Animalitos",
+
+
+
+    paginaResultados:
+
+    "https://www.lottoactivo.com/resultados/animalitos/",
+
+
+
+    metodo:
+
+    "web",
+
+
+
+    actualizacionAutomatica:
+
+    true,
+
+
+
+    activo:
+
+    true
+
+
+
+};
+
+
+
+console.log(
+"Fuente de resultados Lotto Activo cargada",
+window.LOTTO_ACTIVO_RESULTADOS
+);
