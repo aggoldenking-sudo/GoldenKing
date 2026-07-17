ca7142f6909416fafb53a42fbbcd91dccb0377f9
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-17 13:45:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,1 @@
-// Función simple de login
-function intentarLogin() {
-    // Aquí luego integraremos una validación real
-    alert("Bienvenido al sistema");
-    cambiarVista('dashboard');
-}
 
-// Función para cambiar de vistas
-function cambiarVista(vistaId) {
-    document.querySelectorAll('.view').forEach(v => v.style.display = 'none');
-    document.getElementById(vistaId).style.display = 'block';
-}
-
-// Lógica de carga de juegos (dinámica)
-function cargarJuego(nombreJuego) {
-    const area = document.getElementById('area-juego');
-    area.innerHTML = `<h2>Estás en: ${nombreJuego.toUpperCase()}</h2>
-                      <p>Aquí se cargarán los animales o números para apostar.</p>`;
-}
