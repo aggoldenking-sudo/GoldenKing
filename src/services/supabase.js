2c3c3de75e203014a55f8a57f243632765accb0d
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-20 10:52:29 -0400

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,1 +1,61 @@
+/*
+==================================================
+ GOLDEN KING v3
+ SUPABASE CONNECTION
+==================================================
+*/
 
+
+// Configuración Supabase
+
+const SUPABASE_URL = 
+"https://atdxeuxrjepiumpxcqxt.supabase.co";
+
+
+const SUPABASE_KEY = 
+"sb_publishable_wKsM2OWm4USke2jZ1fl0qw_M2TIR3S6";
+
+
+
+
+// Crear cliente
+
+let supabaseClient = null;
+
+
+
+export function getSupabase(){
+
+
+    if(!supabaseClient){
+
+
+        supabaseClient = window.supabase.createClient(
+
+            SUPABASE_URL,
+
+            SUPABASE_KEY
+
+        );
+
+
+    }
+
+
+    return supabaseClient;
+
+
+}
+
+
+
+
+// Exportar configuración
+
+export {
+
+    SUPABASE_URL,
+
+    SUPABASE_KEY
+
+};
