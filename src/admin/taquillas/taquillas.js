64e73bb6c11088815608599258cac385ae30452f
aggoldenking-sudo <aggoldenking@gmail.com>
2026-07-20 13:27:10 -0400

    Update taquillas.js

--- src/admin/taquillas/taquillas.js
+++ src/admin/taquillas/taquillas.js
@@ -1,19 +1,1 @@
-console.log("INICIO TAQUILLAS JS");
 
-
-const lista = document.getElementById("listaTaquillas");
-
-
-console.log("ELEMENTO LISTA:", lista);
-
-
-
-if(lista){
-
-    lista.innerHTML = "JAVASCRIPT ENCONTRO LA LISTA";
-
-}else{
-
-    alert("NO ENCUENTRO listaTaquillas");
-
-}
